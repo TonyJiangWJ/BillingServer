5e14f47e19aafe35693047b36acfd44ecbb4a127
TonyJiangWJ <TonyJiangWJ@gmail.com>
2017-07-16 23:35:03 +0800

    c

--- common/src/main/java/com/tony/billing/util/AuthUtil.java
+++ common/src/main/java/com/tony/billing/util/AuthUtil.java
@@ -1,10 +1,17 @@
 package com.tony.billing.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.tony.billing.util.JavaWebToken.verifyJavaWebToken;
 
 /**
  * Author by TonyJiang on 2017/7/1.
@@ -21,7 +28,8 @@ public class AuthUtil {
     }
 
     public static String getUserTokenId(String token) throws Exception {
-        return (String) getClientLoginInfo(token).get("tokenId");
+        Map<String, Object> sessionInfoMap = getClientLoginInfo(token);
+        return sessionInfoMap == null ? "" : (String) sessionInfoMap.get("tokenId");
     }
 
     public static void setCookieToken(String tokenId, HttpServletResponse response) {
@@ -34,12 +42,38 @@ public class AuthUtil {
     /**
      * session解密
      */
-    public static Map<String, Object> decodeSession(String token) {
+    private static Map<String, Object> decodeSession(String token) {
         try {
-            return verifyJavaWebToken(token);
+            return JavaWebToken.verifyJavaWebToken(token);
         } catch (Exception e) {
             System.err.println("");
             return null;
+        }
+    }
+
+
+    static class JavaWebToken {
+        private static Logger log = LoggerFactory.getLogger(JavaWebToken.class);
+
+        private static Key getKeyInstance() {
+//        return MacProvider.generateKey();
+            //We will sign our JavaWebToken with our ApiKey secret
+            SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+            byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary("springboot");
+            return new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+        }
+
+        private static String createJavaWebToken(Map<String, Object> claims) {
+            return Jwts.builder().setClaims(claims).signWith(SignatureAlgorithm.HS256, getKeyInstance()).compact();
+        }
+
+        private static Map<String, Object> verifyJavaWebToken(String jwt) {
+            try {
+                return Jwts.parser().setSigningKey(getKeyInstance()).parseClaimsJws(jwt).getBody();
+            } catch (Exception e) {
+                log.error("json web token verify failed");
+                return null;
+            }
         }
     }
 }
