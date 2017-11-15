0320144b9fd5730dacb56e7fc409bec4539855f6
jiangwj20966 <jiangwj20966@hundsun.com>
2017-11-15 17:07:21 +0800

    add report

--- web/src/main/java/com/tony/billing/controller/CostReportController.java
+++ web/src/main/java/com/tony/billing/controller/CostReportController.java
@@ -4,11 +4,13 @@ import com.alibaba.fastjson.JSON;
 import com.tony.billing.entity.ReportEntity;
 import com.tony.billing.dto.ReportDTO;
 import com.tony.billing.request.BaseRequest;
+import com.tony.billing.request.costreport.CostReportRequest;
 import com.tony.billing.response.costrecord.ReportResponse;
 import com.tony.billing.service.CostReportService;
 import com.tony.billing.util.BeanCopyUtil;
 import com.tony.billing.util.ResponseUtil;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,7 @@ import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -24,34 +27,51 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/bootDemo")
-public class CostReportController {
+public class CostReportController extends BaseController {
 
     @Resource
     private CostReportService costReportService;
 
     @RequestMapping(value = "/report/get")
-    public ReportResponse getReport(@ModelAttribute("request") BaseRequest request) {
+    public ReportResponse getReport(@ModelAttribute("request") CostReportRequest request) {
         ReportResponse response = new ReportResponse();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
-        Calendar calendar = Calendar.getInstance();
-        int ngm = -6;
-        if (request.getUserId().equals(2L)) {
-            Calendar calendarMonth2 = Calendar.getInstance();
-            calendarMonth2.set(2017, Calendar.FEBRUARY, 1);
-            ngm = calendarMonth2.get(Calendar.MONTH) - calendar.get(Calendar.MONTH) - 1;
-        }
-        calendar.add(Calendar.MONTH, ngm);
-        List<String> monthList = new ArrayList<>();
-        for (int i = 0; i < -ngm; i++) {
-            calendar.add(Calendar.MONTH, 1);
-            monthList.add(sdf.format(calendar.getTime()));
-        }
-        List<ReportEntity> reportList = costReportService.getMonthReportByMonth(monthList, request.getUserId());
-        if (CollectionUtils.isEmpty(reportList)) {
-            ResponseUtil.dataNotExisting(response);
-        } else {
-            response.setReportList(BeanCopyUtil.copy(reportList, ReportDTO.class));
-            ResponseUtil.success(response);
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
+            Calendar calendar = Calendar.getInstance();
+            List<String> monthList = new ArrayList<>();
+            if (StringUtils.isEmpty(request.getStartMonth()) || StringUtils.isEmpty(request.getEndMonth())) {
+                int ngm = -6;
+                if (request.getUserId().equals(2L)) {
+                    Calendar calendarMonth2 = Calendar.getInstance();
+                    calendarMonth2.set(2017, Calendar.FEBRUARY, 1);
+                    ngm = calendarMonth2.get(Calendar.MONTH) - calendar.get(Calendar.MONTH) - 1;
+                }
+                calendar.add(Calendar.MONTH, ngm);
+                for (int i = 0; i < -ngm; i++) {
+                    calendar.add(Calendar.MONTH, 1);
+                    monthList.add(sdf.format(calendar.getTime()));
+                }
+            } else {
+                Date startDate = sdf.parse(request.getStartMonth());
+                Date endDate = sdf.parse(request.getEndMonth());
+                calendar.setTime(startDate);
+                Date tempDate;
+                do {
+                    monthList.add(sdf.format(calendar.getTime()));
+                    calendar.add(Calendar.MONTH, 1);
+                    tempDate = calendar.getTime();
+                } while (tempDate.compareTo(endDate) <= 0);
+            }
+            List<ReportEntity> reportList = costReportService.getMonthReportByMonth(monthList, request.getUserId());
+            if (CollectionUtils.isEmpty(reportList)) {
+                ResponseUtil.dataNotExisting(response);
+            } else {
+                response.setReportList(BeanCopyUtil.copy(reportList, ReportDTO.class));
+                ResponseUtil.success(response);
+            }
+        } catch (Exception e) {
+            logger.error("/report/get error", e);
+            ResponseUtil.sysError(response);
         }
         return response;
     }
--- web/src/main/java/com/tony/billing/filters/FilterDemo.java
+++ web/src/main/java/com/tony/billing/filters/FilterDemo.java
@@ -2,10 +2,10 @@ package com.tony.billing.filters;
 
 import com.tony.billing.filters.wapper.TokenServletRequestWrapper;
 import org.apache.commons.lang3.StringUtils;
+
 import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
@@ -32,9 +32,9 @@ public class FilterDemo extends OncePerRequestFilter {
         }
 
         TokenServletRequestWrapper request = new TokenServletRequestWrapper(httpServletRequest);
-        synchronized (this) { // 在并发访问的时候过滤器链处理请求容易导致并发问题
+//        synchronized (this) { // 在并发访问的时候过滤器链处理请求容易导致并发问题
             filterChain.doFilter(request, httpServletResponse);
-        }
+//        }
 
     }
 }
