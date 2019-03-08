2b73e25ec6e153af706184408dcda7cc689ce3d5
tonyjiangwj <tonyjiangwj@gmail.com>
2019-03-08 15:18:51 +0800

    修改部分接口

--- common/src/main/java/com/tony/billing/util/RedisUtils.java
+++ common/src/main/java/com/tony/billing/util/RedisUtils.java
@@ -1,7 +1,6 @@
 package com.tony.billing.util;
 
 import com.alibaba.fastjson.JSON;
-import com.sun.istack.internal.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
